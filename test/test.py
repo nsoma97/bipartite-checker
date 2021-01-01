0070d8b3c4215002e196bc27abc0cc6b025f7088
Soma Nagy <nagysomabalint@gmail.com>
2021-01-01 20:04:57 +0100

    Added test cases

---
+++ test/test.py
@@ -0,0 +1,23 @@
+from bipartite_checker.checker import is_bipartite
+from bipartite_checker.graph import Graph
+import pytest
+
+expected = [
+    ([[1, 2], [2, 3], [3, 1]], False),
+    ([[1, 2], [2, 3], [3, 4], [4, 1]], True),
+    ([[1, 2], [2, 3], [3, 4], [4, 1], [5, 5]], False),
+    ([[1, 2], [2, 3], [3, 4], [4, 1], [5, 6], [6, 7], [7, 5]], False),
+    ([[1, 2], [2, 3], [3, 4], [4, 1], [5, 6], [6, 7], [7, 8], [8, 5]], True),
+    ([[1, 2], [1, 3], [1, 4]], True),
+    ([[1, 2], [1, 3], [1, 4], [2, 5], [3, 5], [4, 5], [5, 6]], True),
+    ([[1, 2], [1, 3], [1, 4], [2, 5], [3, 5], [4, 5], [5, 1]], False),
+    ([[1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [5, 6]], False),
+    ([[1, 2], [2, 3], [3, 4], [4, 1], [4, 5], [5, 6], [1, 6]], True),
+    (list(zip(list(range(1000)), list(range(1, 1001)))), True)
+]
+
+
+@pytest.mark.parametrize("edge_list,expected", expected)
+def test_is_bipartite(edge_list, expected):
+    g = Graph().from_edges(edge_list)
+    assert expected == is_bipartite(g)
