afd68da516f773a2e0b40b6a70e481349b09a25c
Soma Nagy <nagysomabalint@gmail.com>
2021-01-03 21:09:06 +0100

    Added safety measure

--- bipartite_checker/drawer.py
+++ bipartite_checker/drawer.py
@@ -9,11 +9,13 @@ def draw_bipartite(graph: Graph):
     Draws bipartite coloring.
     :param graph: Graph object.
     """
-    is_bipartite(graph)
-    nx_graph = nx.Graph()
+    bipartite_flag = is_bipartite(graph)
 
-    for u_v, v_v in graph.edges:
-        nx_graph.add_edge(u_v, v_v)
+    if bipartite_flag:
+        nx_graph = nx.Graph()
 
-    nx.draw(nx_graph, node_color=[c.value.lower() for c in graph.node_colors], with_labels=True)
-    plt.savefig('out.png', format='PNG')
+        for u_v, v_v in graph.edges:
+            nx_graph.add_edge(u_v, v_v)
+
+        nx.draw(nx_graph, node_color=[c.value.lower() for c in graph.node_colors], with_labels=True)
+        plt.savefig('out.png', format='PNG')
